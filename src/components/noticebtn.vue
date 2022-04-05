9e6131a614361c45605169f16147d3aff3e12e94
yuxiaoqian2077 <1182426425@qq.com>
2022-04-05 21:29:39 +0800

    commit

--- src/components/noticebtn.vue
+++ src/components/noticebtn.vue
@@ -5,9 +5,9 @@
     
     <el-button type="primary" @click="open1">
     <el-icon style="vertical-align: middle;">
-      <Message />
+      <shopping-cart />
     </el-icon>
-    <span style="vertical-align: middle;"> notice </span>
+    <span style="vertical-align: middle;"> 购买 </span>
   </el-button>
     
     
@@ -20,11 +20,11 @@
 import {ElNotification} from 'element-plus'
 import {ElIcon} from 'element-plus'
 import { h } from 'vue';
-import {Message} from '@element-plus/icons-vue'
+import {ShoppingCart} from '@element-plus/icons-vue'
 const open1 = () => {
   ElNotification.success({
-    title: 'notice',
-    message: h('i', { style: 'color: green' }, 'notice'),
+    title: '成功',
+    message: h('p', { style: 'color: green' }, '购买成功！'),
     duration: 2000,
   })
 }
